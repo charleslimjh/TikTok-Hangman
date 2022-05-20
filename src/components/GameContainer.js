8fe78a1338d3f8bad9257003fee0d58d8c550dbd
Charles Lim <45153859+rei-charl@users.noreply.github.com>
2022-05-21 03:20:20 +0800

    Word generator & displaying words' blanks + hints

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import masterList from "./masterList";
+
+
 function GameContainer() {
-  return <div>Hello World!</div>;
+  const [word, setWord] = useState("");
+  const [hint, setHint] = useState("");
+  const [wordStatus, setWordStatus] = useState([]);
+
+  function generateWordAndHint() {
+    return masterList[Math.floor(Math.random() * masterList.length + 1)];
+  }
+
+  useEffect(() => {
+    const wordHintPair = generateWordAndHint();
+    const word = wordHintPair[0];
+    setWord(word);
+    setHint(wordHintPair[1]);
+
+    const blanks = [];
+    for (let i = 0; i < word.length; i++) {
+      blanks.push("_ ");
+    }
+    setWordStatus(blanks);
+  }, []);
+
+  console.log(word, hint, wordStatus);
+
+  return (
+    <div>
+      <div>Word: {wordStatus}</div>
+      <div>Hint: {hint}</div>
+    </div>
+  );
 }
 
 export default GameContainer;
