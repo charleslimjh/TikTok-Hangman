c4005431b5dbc792f41d65fdcb4651603907c4e6
Charles Lim <charleslimjh@u.nus.edu>
2022-05-26 17:04:32 +0800

    cleaned up guess function

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,53 +1,84 @@
 import { useEffect, useState } from "react";
+import LivesCounter from "./LivesCounter";
 import masterList from "./masterList";
-
 
 function GameContainer() {
   const [word, setWord] = useState("");
   const [hint, setHint] = useState("");
-  const [wordStatus, setWordStatus] = useState([]);
-  const [isGuessed, setGuessed] = useState(false);
+  const [wordStatus, setWordStatus] = useState("");
+  const [livesLeft, setLivesLeft] = useState(6);
 
   function generateWordAndHint() {
     return masterList[Math.floor(Math.random() * masterList.length + 1)];
   }
 
+  // guess function
   function guess(char) {
-    const positions = [];
+    // generate index of characters that match input char
+    let positions = [];
+    let correctGuess = false;
+
     for (let i = 0; i < word.length; i++) {
       if (char === word.charAt(i)) {
         positions.push(i);
+        correctGuess = true;
       }
     }
 
-    const newStatus = wordStatus.slice();
-    for (const i of positions) {
-      newStatus[i] = char + " ";
+    // update wordStatus (blanks)
+    let newStatus = wordStatus.split(" ");
+    for (let i of positions) {
+      newStatus[i] = char;
     }
-    
-    setWordStatus(newStatus);
-    setGuessed(false);
+    let blanks = "";
+    for (let i = 0; i < word.length; i++) {
+      if (newStatus[i] === " ") {
+        blanks += "    ";
+      } else {
+        blanks += (newStatus[i] + " ");
+      }
+    }
+    setWordStatus(blanks);
+
+    // update LivesLeft
+    if (!correctGuess) {
+      setLivesLeft(livesLeft - 1);
+    }
   }
 
+  // Initialize variables
   useEffect(() => {
     const wordHintPair = generateWordAndHint();
     const word = wordHintPair[0];
     setWord(word);
     setHint(wordHintPair[1]);
 
-    const blanks = [];
+    let blanks = "";
     for (let i = 0; i < word.length; i++) {
-      blanks.push("_ ");
+      if (word[i] === " ") {
+        blanks += "    ";
+      } else {
+        blanks += "_ ";
+      }
     }
     setWordStatus(blanks);
   }, []);
 
-  console.log(word, hint, wordStatus);
+  console.log(word, hint, livesLeft);
+  console.log(wordStatus);
 
   return (
     <div>
-      <div>Word: {wordStatus}</div>
+      <div className="word">
+        <pre>Word: {wordStatus}</pre>
+      </div>
       <div>Hint: {hint}</div>
+      <div>
+        <LivesCounter livesLeft={livesLeft} />
+      </div>
+      <div>
+        <button onClick={() => guess("A")}>Click me!</button>
+      </div>
     </div>
   );
 }
