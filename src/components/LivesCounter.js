0c1bfe01daf77cd5008eec174d146dd03874fe74
Charles Lim <charleslimjh@u.nus.edu>
2022-05-27 03:55:55 +0800

    removed unused import

--- src/components/LivesCounter.js
+++ src/components/LivesCounter.js
@@ -1,9 +1,12 @@
+import Canvas from "./Canvas";
+
 function LivesCounter(props) {
   //TODO LIFE COUNTER ANIMATION
 
   return (
     <div>
       <div>You have {props.livesLeft} lives left.</div>
+      <Canvas />
     </div>
   );
 }
